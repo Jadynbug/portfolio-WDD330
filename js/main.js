9417200ef95e5eaf6ab9713f7485d3511abac11d
Jadynbug <83982927+Jadynbug@users.noreply.github.com>
2022-04-21 07:54:31 -0600

    fixing error in js
    added the closing tag for the html maker in js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function listLinks () {
     var len = links.length;
     var fullContent = "";
     for (var i = 0; i < len; i++) {
-        var content = '<li><a href="' + links[i]["url"] + '">' + links[i]["label"]
+        var content = '<li><a href="' + links[i]["url"] + '">' + links[i]["label"] + "</li>";
         fullContent += content;
     }
     document.getElementById("link_list").innerHTML = fullContent;
