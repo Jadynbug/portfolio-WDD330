81bb4b6fdf7d5845981e24d6ee229a3dc822f1c1
Jadynbug <83982927+Jadynbug@users.noreply.github.com>
2022-05-28 20:52:47 -0500

    Update main.js

--- Todo/main.js
+++ Todo/main.js
@@ -2,9 +2,9 @@ import Todo from "./Todo.js";
 import onTouch from "./utilities.js";
 
 
-let toDos = new Todo("test-1", "task-list");
+let toDos = new Todo("Task 1", "test-1", "task-list");
 
 document.getElementById("addTask").addEventListener("click", toDos.addTodo());
-onTouch("#all", toDos.filterList("all"));
+onTouch("all", toDos.filterList("all"));
 onTouch("active", toDos.filterList("active"));
 onTouch("completed", toDos.filterList("completed"));
