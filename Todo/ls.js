aab7c4ce8524ce003ded4b025efc6f117876fbf4
Jadynbug <83982927+Jadynbug@users.noreply.github.com>
2022-05-30 22:04:09 -0500

    Update ls.js

--- Todo/ls.js
+++ Todo/ls.js
@@ -2,7 +2,7 @@
  JSON @param {string} key The key under which the 
  value is stored under in LS
 @return {array} The value as an array of objects*/
-export default function readFromLS(key) {
+export function readFromLS(key) {
     let seri = localStorage.getItem(key);
     console.log("read from ls");
     if (seri != null) {
