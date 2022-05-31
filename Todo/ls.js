0255bf8405ed19eda9c63bef45c88f9a5a8b3fc0
Jadynbug <83982927+Jadynbug@users.noreply.github.com>
2022-05-30 22:07:07 -0500

    Update ls.js

--- Todo/ls.js
+++ Todo/ls.js
@@ -17,9 +17,13 @@ export function readFromLS(key) {
 /*write an array of objects to local storage under the provided
  key @param {string} key The key under which the value is stored under in LS
 * @param {array} data The information to be stored as an array of objects. Must be serialized.*/
-export default function writeToLS(key, data) {
+export function writeToLS(key, data) {
     let seri = JSON.stringify(data);
     alert(seri + " writing to ls " + key);
     localStorage.setItem(key, seri);
     console.log("write to ls " + key);
+}
+
+export default function stuff(key, data = '') {
+    console.log("You called stuff " + key + " " + data);
 }
